68bde063d51be2286f36a1d5400698c30adbcc17
Andy Brown <AndyBrown@example.com>
2025-12-30 14:03:06 +0800

    Upload Parser.hpp

---
+++ SecChem/Utility/Parser.hpp
@@ -0,0 +1,124 @@
+//
+// Created by Andy on 12/29/2025.
+//
+
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
+#if __has_include(<nlohmann/json.hpp>)
+#include <nlohmann/json.hpp>
+#endif
+
+namespace SecUtility
+{
+	template <typename T>
+	T Parse(const std::string& string);
+
+	template <>
+	inline int Parse(const std::string& string)
+	{
+		return std::stoi(string);
+	}
+
+	template <>
+	inline long Parse(const std::string& string)
+	{
+		return std::stol(string);
+	}
+
+	template <>
+	inline unsigned long Parse(const std::string& string)
+	{
+		return std::stoul(string);
+	}
+
+	template <>
+	inline long long Parse(const std::string& string)
+	{
+		return std::stoll(string);
+	}
+
+	template <>
+	inline unsigned long long Parse(const std::string& string)
+	{
+		return std::stoull(string);
+	}
+
+	template <>
+	inline double Parse(const std::string& string)
+	{
+		return std::stod(string);
+	}
+
+	template <>
+	inline float Parse(const std::string& string)
+	{
+		return std::stof(string);
+	}
+
+	template <>
+	inline long double Parse(const std::string& string)
+	{
+		return std::stold(string);
+	}
+
+	template <>
+	inline bool Parse(const std::string& string)
+	{
+		if (string == "0" || string == "false")
+		{
+			return false;
+		}
+
+		if (string == "1" || string == "true")
+		{
+			return true;
+		}
+
+		throw std::runtime_error("Can't parse \"" + string + "\" to bool");
+	}
+
+#if defined(NLOHMANN_JSON_VERSION_MAJOR)
+	template <typename T>
+	T Parse(const nlohmann::json& json)
+	{
+		return json.get<T>();
+	}
+
+	template <>
+	inline double Parse(const nlohmann::json& json)
+	{
+		return std::stod(json.get<std::string>());
+	}
+
+	template <>
+	inline float Parse(const nlohmann::json& json)
+	{
+		return std::stof(json.get<std::string>());
+	}
+
+	template <>
+	inline long double Parse(const nlohmann::json& json)
+	{
+		return std::stold(json.get<std::string>());
+	}
+#endif
+
+	namespace Detail
+	{
+		template <typename Result>
+		struct ParseToFunctor
+		{
+			template <typename Input>
+			friend Result operator|(const Input& input, ParseToFunctor) noexcept
+			{
+				return Parse<Result>(input);
+			}
+		};
+	}  // namespace Detail
+
+	template <typename Result>
+	inline constexpr Detail::ParseToFunctor<Result> ParseTo;
+}  // namespace SecUtility
