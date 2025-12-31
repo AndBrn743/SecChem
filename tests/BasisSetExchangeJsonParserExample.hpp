593f934f71658a10debf1ea35968449cdee55269
Andy Brown <AndyBrown@example.com>
2025-12-31 17:16:34 +0800

    Make the JSON parser example a library (for later reuse)

---
+++ tests/BasisSetExchangeJsonParserExample.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Andy on 12/31/2025.
+//
+
+#pragma once
+
+#include <nlohmann/json.hpp>
+#include "../SecChem/BasisSet/Gaussian.hpp"
+
+SecChem::BasisSet::Gaussian::BasisSet ParseBasisSetExchangeJson(const nlohmann::json& json);
