0ad41868ecf3725c9ba90d2ee0de2836be6a7f1a
Andy Brown <AndyBrown@example.com>
2025-12-31 16:46:58 +0800

    Define molecule classes

--- SecChem/Molecule.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by Andy on 11/22/2025.
-//
---
+++ SecChem/Molecule.hpp
@@ -0,0 +1,243 @@
+//
+// Created by Andy on 11/22/2025.
+//
+
+#pragma once
+
+#include <cassert>
+#include <numeric>
+#include <utility>
+#include <vector>
+
+#include "Utility/IEquatableWithTolerance.hpp"
+
+#include "Atom.hpp"
+
+namespace SecChem
+{
+	template <typename Derived>
+	class IMolecule;
+
+	class Molecule;
+
+	class SharedMolecule;
+
+	template <typename>
+	class Builder;
+}  // namespace SecChem
+
+template <>
+struct SecUtility::Traits<SecChem::Molecule>
+{
+	static constexpr auto DefaultEqualityComparisonTolerance = 1e-12;
+};
+
+namespace SecChem
+{
+	template <typename Derived>
+	class IMolecule
+	{
+	public:
+		std::size_t AtomCount() const noexcept
+		{
+			return std::distance(cbegin(), cend());
+		}
+
+		std::size_t TotalNuclearCharge() const noexcept
+		{
+			return std::accumulate(cbegin(),
+			                       cend(),
+			                       std::size_t{0},
+			                       [](const std::size_t acc, const Atom& atom)
+			                       { return acc + static_cast<std::size_t>(atom.AtomicNumber()); });
+		}
+
+		double Mass() const noexcept
+		{
+			return std::accumulate(
+			        cbegin(), cend(), double{0}, [](const double acc, const Atom& atom) { return acc + atom.Mass(); });
+		}
+
+		const Atom& operator[](const std::size_t i) const noexcept
+		{
+			assert(i < AtomCount());
+			return begin()[i];
+		}
+
+		Atom& operator[](const std::size_t i) noexcept
+		{
+			assert(i < AtomCount());
+			return begin()[i];
+		}
+
+		std::size_t IndexOf(const Atom& atom) const
+		{
+			if (&atom >= cbegin() && atom < cend())
+			{
+				return std::distance(cbegin(), atom);
+			}
+
+			if (const auto it = std::find(cbegin(), cend(), atom); it != cend())
+			{
+				return std::distance(cbegin(), it);
+			}
+
+			throw std::out_of_range("Atom not found");
+		}
+
+		auto begin() noexcept
+		{
+			return AsDerived().begin_Impl();
+		}
+
+		auto end() noexcept
+		{
+			return AsDerived().end_Impl();
+		}
+
+		auto cbegin() const noexcept
+		{
+			return AsDerived().cbegin_Impl();
+		}
+
+		auto cend() const noexcept
+		{
+			return AsDerived().cend_Impl();
+		}
+
+	private:
+		constexpr const Derived& AsDerived() const noexcept
+		{
+			return *static_cast<const Derived*>(this);
+		}
+
+		constexpr Derived& AsDerived() noexcept
+		{
+			return *static_cast<Derived*>(this);
+		}
+
+		/* CRTP PURE VIRTUAL */ auto begin_Impl() noexcept = delete;
+		/* CRTP PURE VIRTUAL */ auto end_Impl() noexcept = delete;
+		/* CRTP PURE VIRTUAL */ auto cbegin_Impl() const noexcept = delete;
+		/* CRTP PURE VIRTUAL */ auto cend_Impl() const noexcept = delete;
+	};
+
+	class Molecule : public IMolecule<Molecule>
+	{
+		friend IMolecule;
+		friend SharedMolecule;
+
+	public:
+		Molecule() = default;
+
+		template <typename InputIterator>
+		Molecule(InputIterator atomsBegin, const InputIterator atomsEnd) : m_Atoms(atomsBegin, atomsEnd)
+		{
+			/* NO CODE */
+		}
+
+		Molecule(const std::initializer_list<Atom> atoms) : m_Atoms(atoms)
+		{
+			/* NO CODE */
+		}
+
+		explicit Molecule(std::vector<Atom> atoms) : m_Atoms(std::move(atoms))
+		{
+			/* NO CODE */
+		}
+
+		explicit Molecule(const SharedMolecule& sharedMolecule);
+
+
+	private:
+		/* CRTP OVERRIDE */ auto begin_Impl() noexcept
+		{
+			return m_Atoms.begin();
+		}
+
+		/* CRTP OVERRIDE */ auto end_Impl() noexcept
+		{
+			return m_Atoms.end();
+		}
+
+		/* CRTP OVERRIDE */ auto cbegin_Impl() const noexcept
+		{
+			return m_Atoms.cbegin();
+		}
+
+		/* CRTP OVERRIDE */ auto cend_Impl() const noexcept
+		{
+			return m_Atoms.cend();
+		}
+
+		std::vector<Atom> m_Atoms;
+	};
+
+	class SharedMolecule : public IMolecule<SharedMolecule>
+	{
+		friend IMolecule;
+		friend Molecule;
+
+	public:
+		template <typename InputIterator>
+		SharedMolecule(InputIterator atomsBegin, const InputIterator atomsEnd)
+		    : m_AtomsPtr(std::make_shared<std::vector<Atom>>(atomsBegin, atomsEnd))
+		{
+			/* NO CODE */
+		}
+
+		SharedMolecule(const std::initializer_list<Atom> atoms) : m_AtomsPtr(std::make_shared<std::vector<Atom>>(atoms))
+		{
+			/* NO CODE */
+		}
+
+		explicit SharedMolecule(std::vector<Atom> atoms)
+		    : m_AtomsPtr(std::make_shared<std::vector<Atom>>(std::move(atoms)))
+		{
+			/* NO CODE */
+		}
+
+		explicit SharedMolecule(const Molecule& molecule)
+		    : m_AtomsPtr(std::make_shared<std::vector<Atom>>(molecule.m_Atoms))
+		{
+			/* NO CODE */
+		}
+
+		explicit SharedMolecule(Molecule&& molecule)
+		    : m_AtomsPtr(std::make_shared<std::vector<Atom>>(std::move(molecule).m_Atoms))
+		{
+			/* NO CODE */
+		}
+
+	private:
+		// ReSharper disable once CppMemberFunctionMayBeConst
+		/* CRTP OVERRIDE */ auto begin_Impl() noexcept
+		{
+			return m_AtomsPtr->begin();
+		}
+
+		// ReSharper disable once CppMemberFunctionMayBeConst
+		/* CRTP OVERRIDE */ auto end_Impl() noexcept
+		{
+			return m_AtomsPtr->end();
+		}
+
+		/* CRTP OVERRIDE */ auto cbegin_Impl() const noexcept
+		{
+			return m_AtomsPtr->cbegin();
+		}
+
+		/* CRTP OVERRIDE */ auto cend_Impl() const noexcept
+		{
+			return m_AtomsPtr->cend();
+		}
+
+		std::shared_ptr<std::vector<Atom>> m_AtomsPtr;
+	};
+
+	inline Molecule::Molecule(const SharedMolecule& sharedMolecule)
+	    : m_Atoms(sharedMolecule.cbegin(), sharedMolecule.cend())
+	{
+		/* NO CODE */
+	}
+}  // namespace SecChem
